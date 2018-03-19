8454ec742a65b7776adb3b955d37f3ab2724f2d0
Duy Tai Nguyen <duytai.cse@gmail.com>
2018-03-19 14:29:36 +0700

    fix error

--- android/src/main/java/im/shimo/react/albums/RNAlbumsPackage.java
+++ android/src/main/java/im/shimo/react/albums/RNAlbumsPackage.java
@@ -1,7 +1,6 @@
 package im.shimo.react.albums;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -15,11 +14,6 @@ public class RNAlbumsPackage implements ReactPackage {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new RNAlbumsModule(reactContext));
         return modules;
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
     }
 
     @Override
